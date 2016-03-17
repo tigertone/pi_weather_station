70667f1059efc7b8621c3a97d8e6caf21f77b669
tigertone <pi@raspberrypi.(none)>
2016-03-17 22:11:24 +0000

    WIP reading serial data from arduino

--- python_code/read_serial_data.py
+++ python_code/read_serial_data.py
@@ -3,12 +3,10 @@ import serial
 arduino = serial.Serial('/dev/ttyACM0', 115200, timeout = 1)
 
 while 1:
-    
-    tmp =arduino.readline()
 
-    if(tmp == 'start'):
+    if(arduino.readline().strip() == 'start'):
         
-        print(arduino.readline())
-        print(arduino.readline())
-        print(arduino.readline())
+        print(arduino.readline().strip())
+        print(arduino.readline().strip())
+        print(arduino.readline().strip())
 
