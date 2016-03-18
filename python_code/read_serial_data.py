5f8333931c0171b7925e85771ec04b8bf3137dfc
tigertone <ablockeel@gmail.com>
2016-03-18 15:42:19 +0000

    Update read_serial_data.py

--- python_code/read_serial_data.py
+++ python_code/read_serial_data.py
@@ -1,12 +1,33 @@
 import serial
+import MySQLdb
+import time
 
 arduino = serial.Serial('/dev/ttyACM0', 115200, timeout = 1)
+
+# Connect to MySQL
+db = MySQLdb.connect(host="localhost", user="root",passwd="password", db="weather_records")
+cur = db.cursor()
 
 while 1:
 
     if(arduino.readline().strip() == 'start'):
         
-        print(arduino.readline().strip())
-        print(arduino.readline().strip())
-        print(arduino.readline().strip())
+        temp = arduino.readline().strip()
+        pressure = arduino.readline().strip()
+        humidity = arduino.readline().strip()
+        
+        datetime = (time.strftime("%Y-%m-%d ") + time.strftime("%H:%M:%S"))
+        
+        query = """INSERT INTO sensor_readings (datetime,decidegrees,pressure,humidity) VALUES (%s,%s,%s,%s)""",(datetime,temp,pressure,humidity)
+        print query
+        
+        try:
+            cur.execute(query)
+            db.commit()
 
+        except:
+            # Rollback if there is an error
+            db.rollback()
+
+        cur.close()
+        db.close()
