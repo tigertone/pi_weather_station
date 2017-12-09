aef1da8ff92eb79f26c50c723e36580624b4e909
tigertone <ablockeel@gmail.com>
2017-12-09 10:40:47 +0000

    Update read_serial_data.py

--- python_code/read_serial_data.py
+++ python_code/read_serial_data.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # can change to python3 when ready
 # chmod +x read_serial_data.py
 
@@ -7,35 +7,40 @@ import MySQLdb
 import time
 
 
-arduino = serial.Serial('/dev/ttyACM0', 115200, timeout = 1)
+arduino = serial.Serial('/dev/ttyUSB1',baudrate=9600)
 
 while 1:
+    try:
+        if(arduino.in_waiting):
+            command=arduino.readline().strip().decode('ascii')
+            if(command == 'start'):
+                temp = arduino.readline().strip().decode('ascii')
+                humidity = arduino.readline().strip().decode('ascii')
+                pressure = arduino.readline().strip().decode('ascii')
+                datetime = (time.strftime("%Y-%m-%d ") + time.strftime("%H:%M:%S"))
 
-    if(arduino.readline().strip() == 'start'):
-        
-        temp = arduino.readline().strip()
-        humidity = arduino.readline().strip()
-        pressure = arduino.readline().strip()
-        
-        datetime = (time.strftime("%Y-%m-%d ") + time.strftime("%H:%M:%S"))
-        
-        query = """INSERT INTO sensor_data (GMT,decidegrees,pressure,humidity) VALUES (%s,%s,%s,%s)""",(datetime,temp,pressure,humidity)
-        
-        try:
-            # Connect to MySQL
-            db = MySQLdb.connect(host="localhost", user="database_writer",passwd="PASSWORD", db="weather_records")
-            cur = db.cursor()
+                query = """INSERT INTO sensor_data (GMT,decidegrees,pressure,humidity) VALUES(%s,%s,%s,%s)""",(datetime,temp,pressure,humidity)
 
-            try:
-                cur.execute(*query)
-                cur.close()
-                db.commit()
+                try:
+                    # Connect to MySQL
+                    db = MySQLdb.connect(host="localhost", user="database_writer",passwd="PASSWORD", db="weather_records")
+                    cur = db.cursor()
 
-            except:
-                # Rollback if there is an error
-                db.rollback()
-        except:
-            cur.close()
+                    try:
+                        cur.execute(*query)
+                        cur.close()
+                        db.commit()
 
-            
-        db.close()
+                    except:
+                        # Rollback if there is an error
+                        db.rollback()
+                except:
+                    cur.close()
+
+
+                db.close()
+
+    except:
+        pass
+    time.sleep(1)
+
