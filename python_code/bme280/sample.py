04377b58ca2f8d68313c04f2f2e8bd2a801ad43a
tigertone <17597748+tigertone@users.noreply.github.com>
2019-08-20 07:08:12 +0100

    Fixed bug when gaps in recording were not seperated by NaNs. Also removed decimation of data for plotting.

--- python_code/bme280/sample.py
+++ python_code/bme280/sample.py
@@ -1,3 +1,4 @@
+
 # chmod +x read_serial_data.py
 
 from __future__ import print_function
@@ -9,11 +10,17 @@ from datetime import date, datetime, timedelta
 from RF24 import *
 import RPi.GPIO as GPIO
 from struct import *
+import sys
 
-sensor = BME280(t_mode=BME280_OSAMPLE_1, p_mode=BME280_OSAMPLE_1, h_mode=BME280_OSAMPLE_1)
+try:
+    sensor = BME280(t_mode=BME280_OSAMPLE_1, p_mode=BME280_OSAMPLE_1, h_mode=BME280_OSAMPLE_1)
+except Exception as e:
+    print('Internal sensor malfuntion')
+    sensor = False
 
 # Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 8Mhz
 radio = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ)
+
 
 ##########################################
 pipes = [0xF0F0F0F0E1, 0xF0F0F0F0D2]
@@ -35,49 +42,48 @@ count = 0
 newExternalData = False
 while True:
     count = count + 1
-    decidegreesInternal = int(sensor.read_temperature()*10)
-    pressureInternal = int(sensor.read_pressure()/100)
-    humidityInternal = int(sensor.read_humidity())
-    local_datetime = datetime.now()
-    local_date = local_datetime.strftime("%Y-%m-%d ")
-    local_datetime_str = local_date + local_datetime.strftime("%H:%M:%S")
+    if sensor:
+        decidegreesInternal = int(sensor.read_temperature()*10)
+        pressureInternal = int(sensor.read_pressure()/100)
+        humidityInternal = int(sensor.read_humidity())
+        local_datetime = datetime.now()
+        local_date = local_datetime.strftime("%Y-%m-%d ")
+        local_datetime_str = local_date + local_datetime.strftime("%H:%M:%S")
 
     try:
         # Connect to MySQL
         db = MySQLdb.connect(host="localhost", user="database_writer",passwd="PASSWORD", db="weather_records")
         cur = db.cursor()
-        
+
         # If there has been a gap since the last data point insert a row with null values so that chartjs won't join the gaps
         try:
-            
+
             query = """SELECT GMT FROM sensor_data order by id desc limit 1;"""
             cur.execute(query)
 
             if cur.rowcount>0:
                 results = cur.fetchone()
-                
-                if (datetime.now() - results[0]).seconds > 120:
-                    query = """INSERT INTO sensor_data (GMT) VALUES(%s)""",(local_datetime_str)
-                    cur.execute(*query)
+                if (local_datetime - results[0]).seconds > 120:
+                    newDatetime = local_datetime - timedelta(minutes=1)
+                    query = """INSERT INTO sensor_data (GMT) VALUES('{0}');""".format(newDatetime.strftime("%Y-%m-%d %H:%M:%S"))
+                    cur.execute(query)
                     db.commit()
-            
-            
+
+
             query = """SELECT sampledDate FROM dailyExtremes order by id desc limit 1;"""
             cur.execute(query)
 
             if cur.rowcount>0:
                 results = cur.fetchone()
-
-            if (date.today() - results[0]).days >= 2:
-                    query = """INSERT INTO dailyExtremes (sampledDate) VALUES(%s)""", (local_datetime.strftime("%Y-%m-%d "))
-                    print(query)
-                    cur.execute(*query)
+                if (date.today() - results[0]).days >= 2:
+                    newDatetime = local_datetime - timedelta(days=1)
+                    query = """INSERT INTO dailyExtremes (sampledDate) VALUES('{0}');""".format(newDatetime.strftime("%Y-%m-%d "))
+                    cur.execute(query)
                     db.commit()
-                
         except Exception as e:
             print(e)
             db.rollback()
-        
+            quit()
 
         try:
             if (newExternalData == True):
@@ -89,7 +95,7 @@ while True:
         except Exception as e:
             print(e)
             db.rollback()
-
+            sys.exec_info()
         try:
             if (newExternalData == True):
                 query = """SELECT ID, sampledDate, decidegreesInternalHigh, decidegreesInternalLow, humidityInternalHigh, humidityInternalLow, pressureInternalHigh, pressureInternalLow, decidegreesExternalHigh, decidegreesExternalLow, humidityExternalHigh, humidityExternalLow, voltageExternal1 FROM dailyExtremes WHERE sampledDate = CURDATE();"""
@@ -123,10 +129,11 @@ while True:
                         cur.execute(*query)
                         db.commit()
 
-                elif (newExternalData == False and (decidegreesInternal>decidegreesInternalHigh or decidegreesInternal<decidegreesInternalLow or humidityInternal>humidityInternalHigh or humidityInternal<humidityInternalLow or pressureInternal>pressureInternalHigh or pressureInternal<pressureInternalLow)):
-                        query = """UPDATE dailyExtremes SET decidegreesInternalHigh=GREATEST(%s,%s),decidegreesInternalLow=LEAST(%s,%s),pressureInternalHigh=GREATEST(%s,%s),pressureInternalLow=LEAST(%s,%s),humidityInternalHigh=GREATEST(%s,%s),humidityInternalLow=LEAST(%s,%s) WHERE ID=%s""",(decidegreesInternal,decidegreesInternalHigh,decidegreesInternal,decidegreesInternalLow,pressureInternal,pressureInternalHigh,pressureInternal,pressureInternalLow,humidityInternal,humidityInternalHigh,humidityInternal,humidityInternalLow,id)
-                        cur.execute(*query)
-                        db.commit()
+                elif newExternalData == False:
+                        if ((decidegreesInternalHigh is None) or (decidegreesInternal>decidegreesInternalHigh or decidegreesInternal<decidegreesInternalLow or humidityInternal>humidityInternalHigh or humidityInternal<humidityInternalLow or pressureInternal>pressureInternalHigh or pressureInternal<pressureInternalLow)):
+                             query = """UPDATE dailyExtremes SET decidegreesInternalHigh=GREATEST(%s,%s),decidegreesInternalLow=LEAST(%s,%s),pressureInternalHigh=GREATEST(%s,%s),pressureInternalLow=LEAST(%s,%s),humidityInternalHigh=GREATEST(%s,%s),humidityInternalLow=LEAST(%s,%s) WHERE ID=%s""",(decidegreesInternal,decidegreesInternalHigh,decidegreesInternal,decidegreesInternalLow,pressureInternal,pressureInternalHigh,pressureInternal,pressureInternalLow,humidityInternal,humidityInternalHigh,humidityInternal,humidityInternalLow,id)
+                             cur.execute(*query)
+                             db.commit()
             else:
                 if (newExternalData == True):
                     query = """INSERT INTO dailyExtremes (sampledDate,decidegreesInternalHigh,decidegreesInternalLow,pressureInternalHigh,pressureInternalLow,humidityInternalHigh,humidityInternalLow, decidegreesExternalHigh, decidegreesExternalLow, humidityExternalHigh,humidityExternalLow,voltageExternal1) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",(local_date,decidegreesInternal,decidegreesInternal,pressureInternal,pressureInternal,humidityInternal,humidityInternal, decidegreesExternal, decidegreesExternal, humidityExternal, humidityExternal, voltage)
@@ -138,6 +145,9 @@ while True:
         except Exception as e:
             print(e)
             db.rollback()
+            print(sys.exc_info())
+            
+           
 
     except Exception as e:
         print(e)
@@ -166,6 +176,8 @@ while True:
 
         except Exception as e:
             print(e)
+            exec_type, exec_obj, exec_tb = sys.exec_info()
+            print(exec_type, exec_tb.tb_lineno)
 
  
     print("new loop")
